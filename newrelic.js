8554c1444e44808545a5c40d0c3a669696f8db77
rtaimal <rtaimal@gmail.com>
2017-05-01 12:07:38 -0500

    include New Relic

---
+++ newrelic.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['voxworker'],
+  /**
+   * Your New Relic license key.
+   * https://rpm.newrelic.com/accounts/1621847/applications/setup#
+   */
+  license_key: process.env.VOX_NEW_RELIC_KEY,
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+};
